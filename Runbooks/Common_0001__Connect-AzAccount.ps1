991f8374c746e29a7ad1a9ba67288510a1615a58
Julian Pawlowski <75446+jpawlowski@users.noreply.github.com>
2024-03-06 21:00:30 +0100

    suppress warning

--- Runbooks/Common_0001__Connect-AzAccount.ps1
+++ Runbooks/Common_0001__Connect-AzAccount.ps1
@@ -76,10 +76,11 @@ Write-Verbose "---START of $((Get-Item $PSCommandPath).Name), $((Test-ScriptFile
 $StartupVariables = (Get-Variable | & { process { $_.Name } })      # Remember existing variables so we can cleanup ours at the end of the script
 
 #region [COMMON] ENVIRONMENT ---------------------------------------------------
-$env:AZURE_RM_WARN = 'false'
+$WarningPreference = 'SilentlyContinue'
 ./Common_0000__Import-Module.ps1 -Modules @(
     @{ Name = 'Az.Accounts'; MinimumVersion = '2.8'; MaximumVersion = '2.65535' }
 ) 1> $null
+$WarningPreference = 'Continue'
 #endregion ---------------------------------------------------------------------
 
 if (-Not (Get-AzContext)) {
