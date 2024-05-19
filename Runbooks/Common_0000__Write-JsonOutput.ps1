f2e5373fe703b252fdf4cc9267bde15184689d6a
Julian Pawlowski <75446+jpawlowski@users.noreply.github.com>
2024-05-19 20:30:28 +0200

    Add support for converting date properties to ISO8601 string for PowerShell 5.1

--- Runbooks/Common_0000__Write-JsonOutput.ps1
+++ Runbooks/Common_0000__Write-JsonOutput.ps1
@@ -1,5 +1,5 @@
 <#PSScriptInfo
-.VERSION 1.0.0
+.VERSION 1.1.0
 .GUID fd95f377-4c0a-4dfa-addd-14cf6dca99cf
 .AUTHOR Julian Pawlowski
 .COMPANYNAME Workoho GmbH
@@ -12,8 +12,8 @@
 .REQUIREDSCRIPTS
 .EXTERNALSCRIPTDEPENDENCIES
 .RELEASENOTES
-    Version 1.0.0 (2024-02-25)
-    - Initial release.
+    Version 1.1.0 (2024-05-19)
+    - Added support for converting date properties to ISO8601 string for PowerShell 5.1
 #>
 
 <#
@@ -47,6 +47,50 @@ Param(
 
 if (-Not $PSCommandPath) { Write-Error 'This runbook is used by other runbooks and must not be run directly.' -ErrorAction Stop; exit }
 # Write-Verbose "---START of $((Get-Item $PSCommandPath).Name), $((Test-ScriptFileInfo $PSCommandPath | Select-Object -Property Version, Guid | & { process{$_.PSObject.Properties | & { process{$_.Name + ': ' + $_.Value} }} }) -join ', ') ---"
+
+function Convert-DatePropertiesToISO8601String {
+    param(
+        [Parameter(Mandatory = $true)]
+        $InputObject
+    )
+
+    Write-Verbose "Converting date properties to ISO8601 string"
+
+    if ($InputObject -is [Array] -or $InputObject -is [System.Collections.IList]) {
+        foreach ($item in $InputObject) {
+            Convert-DatePropertiesToISO8601String -InputObject $item
+        }
+    }
+    elseif ($InputObject -is [Hashtable] -or $InputObject -is [System.Collections.IDictionary]) {
+        foreach ($key in $InputObject.Keys) {
+            if ($InputObject[$key] -is [DateTime]) {
+                Write-Verbose "Converting date property $key to ISO8601 string"
+                $InputObject[$key] = $InputObject[$key].ToString("yyyy-MM-ddTHH:mm:ssZ")
+            }
+            elseif ($InputObject[$key] -is [PSCustomObject] -or $InputObject[$key] -is [Hashtable] -or $InputObject[$key] -is [System.Collections.IDictionary]) {
+                Convert-DatePropertiesToISO8601String -InputObject $InputObject[$key]
+            }
+        }
+    }
+    else {
+        $properties = $InputObject.PSObject.Properties
+
+        foreach ($property in $properties) {
+            Write-Verbose "Checking property $($property.Name)"
+            if ($property.Value -is [DateTime]) {
+                Write-Verbose "Converting date property $($property.Name) to ISO8601 string"
+                $InputObject.$($property.Name) = $property.Value.ToString("yyyy-MM-ddTHH:mm:ssZ")
+            }
+            elseif ($property.Value -is [PSCustomObject]) {
+                Convert-DatePropertiesToISO8601String -InputObject $property.Value
+            }
+        }
+    }
+}
+
+if ($PSVersionTable.PSVersion.Major -lt 7) {
+    Convert-DatePropertiesToISO8601String -InputObject $InputObject
+}
 
 $params = if ($ConvertToParam) { $ConvertToParam.Clone() } else { @{} }
 if ($null -eq $params.Compress) {
