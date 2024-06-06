871a0df4f7ed0f4f75a6f581aef7f92489eee91c
Julian Pawlowski <75446+jpawlowski@users.noreply.github.com>
2024-06-06 19:49:07 +0200

    Use Invoke-AzRestMethod

--- Runbooks/Common_0001__Connect-AzAccount.ps1
+++ Runbooks/Common_0001__Connect-AzAccount.ps1
@@ -1,5 +1,5 @@
 <#PSScriptInfo
-.VERSION 1.3.0
+.VERSION 1.4.0
 .GUID 1dc765c0-4922-4142-a945-13206df25f13
 .AUTHOR Julian Pawlowski
 .COMPANYNAME Workoho GmbH
@@ -12,8 +12,9 @@
 .REQUIREDSCRIPTS
 .EXTERNALSCRIPTDEPENDENCIES
 .RELEASENOTES
-    Version 1.3.0 (2024-06-04)
-    - Split setting of environment variables into two steps to enforce setting them after connecting to Microsoft Graph only.
+    Version 1.4.0 (2024-06-06)
+    - Use Invoke-AzRestMethod instead of Get-AzAutomationAccount and Get-AzAutomationJob to retrieve Automation Account and Job details.
+    - Add $env:AZURE_AUTOMATION_AccountId
 #>
 
 <#
@@ -24,6 +25,7 @@
     This runbook connects to Azure using either a Managed Service Identity or an interactive session, depending on the execution environment.
 
     The script also retrieves the following information about the current Azure Automation Account and sets them as environment variables:
+    - AZURE_AUTOMATION_AccountId
     - AZURE_AUTOMATION_SubscriptionId
     - AZURE_AUTOMATION_ResourceGroupName
     - AZURE_AUTOMATION_AccountName
@@ -82,6 +84,24 @@ $StartupVariables = (Get-Variable | & { process { $_.Name } })      # Remember e
 #region [COMMON] FUNCTIONS -----------------------------------------------------
 function Set-EnvVarsAfterMgConnect {
     if ('AzureAutomation/' -eq $env:AZUREPS_HOST_ENVIRONMENT -or $PSPrivateMetadata.JobId) {
+        if (
+            $env:AZURE_AUTOMATION_SubscriptionId -and
+            $env:AZURE_AUTOMATION_ResourceGroupName -and
+            $env:AZURE_AUTOMATION_AccountName -and
+            $env:AZURE_AUTOMATION_IDENTITY_PrincipalId -and
+            $env:AZURE_AUTOMATION_IDENTITY_TenantId -and
+            $env:AZURE_AUTOMATION_IDENTITY_Type -and
+            $env:AZURE_AUTOMATION_RUNBOOK_Name -and
+            $env:AZURE_AUTOMATION_RUNBOOK_CreationTime -and
+            $env:AZURE_AUTOMATION_RUNBOOK_LastModifiedTime -and
+            $env:AZURE_AUTOMATION_RUNBOOK_JOB_CreationTime -and
+            $env:AZURE_AUTOMATION_RUNBOOK_JOB_StartTime -and
+            $env:AZURE_AUTOMATION_RUNBOOK_CreationTime -and
+            $env:AZURE_AUTOMATION_RUNBOOK_LastModifiedTime
+        ) {
+            return
+        }
+
         Write-Verbose '[COMMON]: - Running in Azure Automation - Generating connection environment variables'
 
         if ([string]::IsNullOrEmpty($env:MG_PRINCIPAL_DISPLAYNAME)) {
@@ -89,65 +109,72 @@ function Set-EnvVarsAfterMgConnect {
         }
 
         #region [COMMON] ENVIRONMENT ---------------------------------------------------
-        try {
-            if ($null -eq $global:PSModuleAutoloadingPreference) {
-                $null = Get-AutomationVariable -Name DummyVar -ErrorAction SilentlyContinue -WhatIf
+        if ($null -eq (Get-Module -Name Orchestrator.AssetManagement.Cmdlets -ErrorAction SilentlyContinue)) {
+            try {
+                if ($null -eq $global:PSModuleAutoloadingPreference) {
+                    $null = Orchestrator.AssetManagement.Cmdlets\Get-AutomationVariable -Name DummyVar -ErrorAction SilentlyContinue -WhatIf
+                }
+                else {
+                    $AutoloadingPreference = $global:PSModuleAutoloadingPreference
+                    $global:PSModuleAutoloadingPreference = 'All'
+                    $null = Orchestrator.AssetManagement.Cmdlets\Get-AutomationVariable -Name DummyVar -ErrorAction SilentlyContinue -WhatIf
+                    $global:PSModuleAutoloadingPreference = $AutoloadingPreference
+                }
             }
-            else {
-                $AutoloadingPreference = $global:PSModuleAutoloadingPreference
-                $global:PSModuleAutoloadingPreference = 'All'
-                $null = Get-AutomationVariable -Name DummyVar -ErrorAction SilentlyContinue -WhatIf
-                $global:PSModuleAutoloadingPreference = $AutoloadingPreference
+            catch {
+                # Do nothing. We just want to trigger auto import of Orchestrator.AssetManagement.Cmdlets
             }
-        }
-        catch {
-            # Do nothing. We just want to trigger auto import of Orchestrator.AssetManagement.Cmdlets
         }
 
-        ./Common_0000__Import-Module.ps1 -Modules @(
-            @{ Name = 'Az.Automation'; MinimumVersion = '1.7'; MaximumVersion = '1.65535' }
-        ) 1> $null
+        $apiVersion = '2023-11-01'
         #endregion ---------------------------------------------------------------------
 
-        $AzAutomationAccount = Get-AzAutomationAccount -ErrorAction Stop -Verbose:$false | Where-Object { $_.AutomationAccountName -eq $env:MG_PRINCIPAL_DISPLAYNAME }
-        if ($AzAutomationAccount) {
-            Write-Verbose '[COMMON]: - Retrieved Automation Account details'
-            [Environment]::SetEnvironmentVariable('AZURE_AUTOMATION_SubscriptionId', $AzAutomationAccount.SubscriptionId)
-            [Environment]::SetEnvironmentVariable('AZURE_AUTOMATION_ResourceGroupName', $AzAutomationAccount.ResourceGroupName)
-            [Environment]::SetEnvironmentVariable('AZURE_AUTOMATION_AccountName', $AzAutomationAccount.AutomationAccountName)
-            [Environment]::SetEnvironmentVariable('AZURE_AUTOMATION_IDENTITY_PrincipalId', $AzAutomationAccount.Identity.PrincipalId)
-            [Environment]::SetEnvironmentVariable('AZURE_AUTOMATION_IDENTITY_TenantId', $AzAutomationAccount.Identity.TenantId)
-            [Environment]::SetEnvironmentVariable('AZURE_AUTOMATION_IDENTITY_Type', $AzAutomationAccount.Identity.Type)
+        try {
+            $AzAutomationAccount = ((Az.Accounts\Invoke-AzRestMethod -Method Get -Path "/subscriptions/$((Az.Accounts\Get-AzContext).Subscription.Id)/providers/Microsoft.Automation/automationAccounts?api-version=$apiVersion" -ErrorAction Stop).Content | ConvertFrom-Json).Value | Where-Object { $_.name -eq $env:MG_PRINCIPAL_DISPLAYNAME }
+            if ($AzAutomationAccount) {
+                Write-Verbose '[COMMON]: - Retrieved Automation Account details'
+                $null, $null, $subscriptionId, $null, $resourceGroupName, $null, $null, $null, $automationAccountName = $AzAutomationAccount.id -split '/'
+                [Environment]::SetEnvironmentVariable('AZURE_AUTOMATION_AccountId', $AzAutomationAccount.id)
+                [Environment]::SetEnvironmentVariable('AZURE_AUTOMATION_SubscriptionId', $subscriptionId)
+                [Environment]::SetEnvironmentVariable('AZURE_AUTOMATION_ResourceGroupName', $resourceGroupName)
+                [Environment]::SetEnvironmentVariable('AZURE_AUTOMATION_AccountName', $automationAccountName)
+                [Environment]::SetEnvironmentVariable('AZURE_AUTOMATION_IDENTITY_PrincipalId', $AzAutomationAccount.Identity.PrincipalId)
+                [Environment]::SetEnvironmentVariable('AZURE_AUTOMATION_IDENTITY_TenantId', $AzAutomationAccount.Identity.TenantId)
+                [Environment]::SetEnvironmentVariable('AZURE_AUTOMATION_IDENTITY_Type', $AzAutomationAccount.Identity.Type)
 
-            if ($PSPrivateMetadata.JobId) {
+                if ($PSPrivateMetadata.JobId) {
 
-                $AzAutomationJob = Get-AzAutomationJob -ResourceGroupName $AzAutomationAccount.ResourceGroupName -AutomationAccountName $AzAutomationAccount.AutomationAccountName -Id $PSPrivateMetadata.JobId -ErrorAction Stop -Verbose:$false
-                if ($AzAutomationJob) {
-                    Write-Verbose '[COMMON]: - Retrieved Automation Job details'
-                    [Environment]::SetEnvironmentVariable('AZURE_AUTOMATION_RUNBOOK_Name', $AzAutomationJob.RunbookName)
-                    [Environment]::SetEnvironmentVariable('AZURE_AUTOMATION_RUNBOOK_JOB_CreationTime', $AzAutomationJob.CreationTime.ToUniversalTime())
-                    [Environment]::SetEnvironmentVariable('AZURE_AUTOMATION_RUNBOOK_JOB_StartTime', $AzAutomationJob.StartTime.ToUniversalTime())
+                    $AzAutomationJob = (Az.Accounts\Invoke-AzRestMethod -Method Get -Path "$($AzAutomationAccount.id)/jobs/$($PSPrivateMetadata.JobId)?api-version=$apiVersion" -ErrorAction Stop).Content | ConvertFrom-Json
+                    if ($AzAutomationJob) {
+                        Write-Verbose '[COMMON]: - Retrieved Automation Job details'
+                        [Environment]::SetEnvironmentVariable('AZURE_AUTOMATION_RUNBOOK_Name', $AzAutomationJob.properties.runbook.name)
+                        [Environment]::SetEnvironmentVariable('AZURE_AUTOMATION_RUNBOOK_JOB_CreationTime', [DateTime]::Parse($AzAutomationJob.properties.creationTime).ToUniversalTime())
+                        [Environment]::SetEnvironmentVariable('AZURE_AUTOMATION_RUNBOOK_JOB_StartTime', [DateTime]::Parse($AzAutomationJob.properties.startTime).ToUniversalTime())
 
-                    $AzAutomationRunbook = Get-AzAutomationRunbook -ResourceGroupName $AzAutomationAccount.ResourceGroupName -AutomationAccountName $AzAutomationAccount.AutomationAccountName -Name $AzAutomationJob.RunbookName -ErrorAction Stop -Verbose:$false
-                    if ($AzAutomationRunbook) {
-                        Write-Verbose '[COMMON]: - Retrieved Automation Runbook details'
-                        [Environment]::SetEnvironmentVariable('AZURE_AUTOMATION_RUNBOOK_CreationTime', $AzAutomationRunbook.CreationTime.ToUniversalTime())
-                        [Environment]::SetEnvironmentVariable('AZURE_AUTOMATION_RUNBOOK_LastModifiedTime', $AzAutomationRunbook.LastModifiedTime.ToUniversalTime())
+                        $AzAutomationRunbook = (Az.Accounts\Invoke-AzRestMethod -Method Get -Path "$($AzAutomationAccount.id)/runbooks/$($AzAutomationJob.properties.runbook.name)?api-version=$apiVersion" -ErrorAction Stop).Content | ConvertFrom-Json
+                        if ($AzAutomationRunbook) {
+                            Write-Verbose '[COMMON]: - Retrieved Automation Runbook details'
+                            [Environment]::SetEnvironmentVariable('AZURE_AUTOMATION_RUNBOOK_CreationTime', [DateTime]::Parse($AzAutomationRunbook.properties.creationTime).ToUniversalTime())
+                            [Environment]::SetEnvironmentVariable('AZURE_AUTOMATION_RUNBOOK_LastModifiedTime', [DateTime]::Parse($AzAutomationRunbook.properties.lastModifiedTime).ToUniversalTime())
+                        }
+                        else {
+                            Throw "[COMMON]: - Unable to find own Automation Runbook details for runbook name '$($AzAutomationJob.properties.runbook.name)'"
+                        }
                     }
                     else {
-                        Throw "[COMMON]: - Unable to find own Automation Runbook details for runbook name $($AzAutomationJob.RunbookName)"
+                        Throw "[COMMON]: - Unable to find own Automation Job details for job Id $($PSPrivateMetadata.JobId)"
                     }
                 }
                 else {
-                    Throw "[COMMON]: - Unable to find own Automation Job details for job Id $($PSPrivateMetadata.JobId)"
+                    Throw '[COMMON]: - Missing global variable $PSPrivateMetadata.JobId'
                 }
             }
             else {
-                Throw '[COMMON]: - Missing global variable $PSPrivateMetadata.JobId'
+                Throw "[COMMON]: - Unable to find own Automation Account details for '$env:MG_PRINCIPAL_DISPLAYNAME'"
             }
         }
-        else {
-            Throw "[COMMON]: - Unable to find own Automation Account details for '$env:MG_PRINCIPAL_DISPLAYNAME'"
+        catch {
+            Throw "Error setting Azure Automation environment variables: $($_.Exception.Message)"
         }
     }
     else {
@@ -156,14 +183,13 @@ function Set-EnvVarsAfterMgConnect {
 }
 #endregion ---------------------------------------------------------------------
 
-if (Get-AzContext) {
+if (Az.Accounts\Get-AzContext) {
     if ($SetEnvVarsAfterMgConnect -eq $true) {
         try {
             Set-EnvVarsAfterMgConnect
         }
         catch {
-            Write-Error $_.Exception.Message -ErrorAction Stop
-            exit
+            Throw $_
         }
     }
 }
@@ -202,16 +228,15 @@ else {
         if ($Subscription) { $params.Subscription = $Subscription }
 
         Write-Information 'Connecting to Microsoft Azure ...' -InformationAction Continue
-        $Context = (Connect-AzAccount @params).context
-        $Context = Set-AzContext -SubscriptionName $Context.Subscription -DefaultProfile $Context
+        $Context = (Az.Accounts\Connect-AzAccount @params).context
+        $Context = Az.Accounts\Set-AzContext -SubscriptionName $Context.Subscription -DefaultProfile $Context
 
         if ($SetEnvVarsAfterMgConnect -eq $true) {
             Set-EnvVarsAfterMgConnect
         }
     }
     catch {
-        Write-Error "Microsoft Azure connection error: $($_.Exception.Message)" -ErrorAction Stop
-        exit
+        Throw $_
     }
 }
 
